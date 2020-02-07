6710498f55c31b8227eefc2959cd51ae1dc66c61
Schuyler Martin <schuylermartin45@gmail.com>
2020-02-06 19:13:45 -0700

    Added basic color rendering to Text Mode

--- src/gotris/view/textGame.go
+++ src/gotris/view/textGame.go
@@ -23,6 +23,74 @@ type TextGame struct {
 	screen tcell.Screen
 }
 
+// Text Mode Color Enum
+type color uint8
+
+// Text Mode Color enumerations
+const (
+	Blue            color = 0
+	Cyan            color = 1
+	Grey            color = 2
+	Yellow          color = 3
+	Green           color = 4
+	Violet          color = 5
+	Red             color = 6
+	BoardBackground color = 7
+	BoardBorder     color = 8
+	BoardForeground color = 9
+)
+
+/***** Functions *****/
+
+// lookupColor returns the `tcell` color code for a given color
+func lookupColor(clr color) tcell.Style {
+	style := tcell.StyleDefault
+	switch clr {
+	case Blue:
+		return style.Foreground(tcell.ColorBlue).Background(tcell.ColorDarkBlue)
+	case Cyan:
+		return style.Foreground(tcell.ColorLightBlue).Background(tcell.ColorRoyalBlue)
+	case Grey:
+		return style.Foreground(tcell.ColorGrey).Background(tcell.ColorDarkGrey)
+	case Yellow:
+		return style.Foreground(tcell.ColorYellow).Background(tcell.ColorSandyBrown)
+	case Green:
+		return style.Foreground(tcell.ColorGreen).Background(tcell.ColorDarkGreen)
+	case Violet:
+		return style.Foreground(tcell.ColorViolet).Background(tcell.ColorDarkViolet)
+	case Red:
+		return style.Foreground(tcell.ColorRed).Background(tcell.ColorDarkRed)
+	case BoardBackground:
+		return style.Foreground(tcell.ColorCornflowerBlue)
+	case BoardBorder:
+		return style.Foreground(tcell.ColorDarkGrey)
+	case BoardForeground:
+		return style.Foreground(tcell.ColorGrey)
+	}
+	return 0x00
+}
+
+// lookupTileColor maps TileColor to the `tcell` color code
+func lookupTileColor(clr model.TileColor) tcell.Style {
+	switch clr {
+	case model.Blue:
+		return lookupColor(Blue)
+	case model.Cyan:
+		return lookupColor(Cyan)
+	case model.Grey:
+		return lookupColor(Grey)
+	case model.Yellow:
+		return lookupColor(Yellow)
+	case model.Green:
+		return lookupColor(Green)
+	case model.Violet:
+		return lookupColor(Violet)
+	case model.Red:
+		return lookupColor(Red)
+	}
+	return 0x00
+}
+
 /***** Methods *****/
 
 // RenderHelpMenu returns a string to display the help menu in the terminal.
@@ -63,7 +131,7 @@ func (t *TextGame) InitGame(b *model.Board) {
 
 // RenderGame runs the primary gameplay loop.
 func (t *TextGame) RenderGame() {
-	// A digital frontier...
+	// Primary game loop loops until the game completes
 	for {
 		// Advance the game
 		_, endGame := t.board.Next()
@@ -89,21 +157,29 @@ func (t *TextGame) ExitGame(playAgain bool) {
  Draws the current board to the screen.
 */
 func (t TextGame) drawBoard() {
+	const leftPad = 8
+	const topPad = 2
+
+	t.screen.Fill(' ', lookupColor(BoardBackground))
 	workingGrid := t.board.Current()
+	y := topPad
 	for row := 0; row < len(workingGrid); row++ {
+		// `y` includes padding from the top of the screen
 		var mask uint8 = 1 << 7
 		for col := 0; col < int(model.BoardWidth); col++ {
+			// Calculate the left and right block x coordinates
+			xL := leftPad + (2 * col)
+			xR := leftPad + (2 * col) + 1
 			if (workingGrid[row] & mask) > 0 {
-				// TODO draw piece
-				t.screen.SetContent(2*col, row, '█', nil, 32)
-				t.screen.SetContent((2*col)+1, row, '█', nil, 32)
+				t.screen.SetContent(xL, y, '▇', nil, lookupColor(Red))
+				t.screen.SetContent(xR, y, '▇', nil, lookupColor(Red))
 			} else {
-				// TODO draw transparency/background/nothing
-				t.screen.SetContent(2*col, row, 32, nil, 32)
-				t.screen.SetContent((2*col)+1, row, 32, nil, 32)
+				t.screen.SetContent(xL, y, ' ', nil, lookupColor(BoardForeground))
+				t.screen.SetContent(xR, y, ' ', nil, lookupColor(BoardForeground))
 			}
 			mask >>= 1
 		}
+		y++
 	}
 	t.screen.Show()
 }
